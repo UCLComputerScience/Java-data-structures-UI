7da0a88d0576c2240d00dcefe5b9ddbc57832e9b
SteveLim99 <SteveLim99@users.noreply.github.com>
2019-03-09 02:22:20 +0000

    Java CW2 push #3

--- Patient/src/Patient.java
+++ Patient/src/Patient.java
@@ -1,32 +1,8 @@
-
 import java.util.HashMap;
 
 
 public class Patient {
     HashMap<String,String> hmap;
-    private String id;
-
-
-
-    private String birthDate;
-    private String deathDate;
-    private String ssn;
-    private String drivers;
-    private String passport;
-    private String prefix;
-    private String first;
-    private String last;
-    private String suffix;
-    private String maiden;
-    private String marital;
-    private String race;
-    private String ethnicity;
-    private String gender;
-    private String birthPlace;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
 
     public void setID(String id)
     {
@@ -35,8 +11,7 @@ public class Patient {
 
     public String getID()
     {
-        String id = hmap.get("ID:");
-        return id;
+        return hmap.get("ID:");
     }
 
     public void setBirthDate(String date)
@@ -79,4 +54,153 @@ public class Patient {
         return hmap.get("Drivers:");
     }
 
+    public void setPassport(String passport)
+    {
+        hmap.put("Passport:",passport);
+    }
+
+    public String getPassport()
+    {
+        return hmap.get("Passport:");
+    }
+
+    public void setPrefix(String prefix)
+    {
+        hmap.put("Prefix:",prefix);
+    }
+
+    public String getPrefix()
+    {
+        return hmap.get("Prefix:");
+    }
+
+    public void setFirst(String first)
+    {
+        hmap.put("First:",first);
+    }
+
+    public String getFirst()
+    {
+        return hmap.get("First:");
+    }
+
+    public void setLast(String last)
+    {
+        hmap.put("Last:",last);
+    }
+
+    public String getLast()
+    {
+        return hmap.get("Last:");
+    }
+
+    public void setSuffix(String suffix)
+    {
+        hmap.put("Suffix:",suffix);
+    }
+
+    public String getSuffix()
+    {
+        return hmap.get("Suffix:");
+    }
+
+    public void setMaiden(String maiden)
+    {
+        hmap.put("Maiden",maiden);
+    }
+
+    public String getMaiden()
+    {
+        return hmap.get("Maiden:");
+    }
+
+    public void setMarital(String marital)
+    {
+        hmap.put("Marital:",marital);
+    }
+
+    public String getMarital()
+    {
+        return hmap.get("Marital:");
+    }
+
+    public void setRace(String race)
+    {
+        hmap.put("Race:",race);
+    }
+
+    public String getRace()
+    {
+        return hmap.get("Race:");
+    }
+
+    public void setEthnicity(String ethnicity)
+    {
+        hmap.put("Ethnicity:",ethnicity);
+    }
+
+    public String getEthnicity()
+    {
+        return hmap.get("Ethnicity:");
+    }
+
+    public void setGender(String gender)
+    {
+        hmap.put("Gender:",gender);
+    }
+
+    public String getGender()
+    {
+        return hmap.get("Gender:");
+    }
+
+    public void setBirthPlace(String birthPlace)
+    {
+        hmap.put("BirthPlace:",birthPlace);
+    }
+
+    public String getBirthPlace()
+    {
+        return hmap.get("BirthPlace:");
+    }
+
+    public void setAddress(String address)
+    {
+        hmap.put("Address:",address);
+    }
+
+    public String getAddress()
+    {
+        return hmap.get("Address:");
+    }
+
+    public void setCity(String city)
+    {
+        hmap.put("City:",city);
+    }
+
+    public String getCity()
+    {
+        return hmap.get("City:");
+    }
+
+    public void setState(String state)
+    {
+        hmap.put("State:",state);
+    }
+
+    public String getState()
+    {
+        return hmap.get("State:");
+    }
+
+    public void setZip(String zip)
+    {
+        hmap.put("Zip:",zip);
+    }
+
+    public String getZip()
+    {
+        return hmap.get("Zip:");
+    }
 }
