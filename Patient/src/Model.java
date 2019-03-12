5105f1744af89c068898ff38c6658646c45272a3
SteveLim99 <SteveLim99@users.noreply.github.com>
2019-03-12 00:19:15 +0000

    Java CW2 PushBranch#10
    Implementing Model Class

--- Patient/src/Model.java
+++ Patient/src/Model.java
@@ -1,2 +1,10 @@
+import java.util.List;
+import java.util.
+
 public class Model {
+    ReadCSV files = new ReadCSV();
+
+    public List readFile(){
+        List<Patient> str = files.ReadCSV();
+    }
 }
